0cb08897becebaf92c7867c456b6e343c74e664f
Narayan <Shashankhegde32@gmaol.com>
2020-06-10 23:26:17 +0530

    Random Numbers Generated

--- empWage.sh
+++ empWage.sh
@@ -1,3 +1,10 @@
-#! /bin/bash +x
+#! /bin/bash -x
 
 echo "Welcome to Employee Wage Calculation"
+declare -a empAttendance
+for ((i=0;i<=20;i++))
+do
+	ran=`expr ${RANDOM:0:1} \/ 5`
+	empAttendance[$i]=$ran
+done
+echo "${empAttendance[@]}"
