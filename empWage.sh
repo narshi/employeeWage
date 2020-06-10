dd3c73303460966332655b5e481a8bcfbd0db831
Narayan <Shashankhegde32@gmaol.com>
2020-06-10 23:57:42 +0530

    Wage calculation

--- empWage.sh
+++ empWage.sh
@@ -8,3 +8,29 @@ do
 	empAttendance[$i]=$ran
 done
 echo "${empAttendance[@]}"
+
+	echo "Enter Method of salaty hourly/daily/monthly"
+	read Wage
+	if [ $isPresent == 1 ]
+	then
+		case "$Wage" in
+			"daily")
+				dSalary=`expr $wPhr \* $workHour \* 20`
+				echo "$dSalary"
+			;;
+			"monthly")
+				mSalary=`expr $wPhr \* $workHour \* 12`
+				echo "$mSalary"
+			;;
+			"hourly")
+				hSalary=`expr $wPhr \* $workHour`
+				echo "$hSalary"
+			;;
+			*)
+				echo "Employee not exists"
+			;;
+		esac
+	else
+		echo "Employee not exists"
+	fi
+fi
